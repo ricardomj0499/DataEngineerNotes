6098136bf2c1625f552fcb26b3c40e0f8ce89658
ricardomj0499 <ricardomj0499@hotmail.com>
2025-02-10 20:55:31 -0600

    notes on what to do next and references

--- basic_algorithms_and_data_structures/__init__.py
+++ basic_algorithms_and_data_structures/__init__.py
@@ -4,6 +4,16 @@ Array, fixed lenght
 list, dinamically store, some types
 Will do with some types on only one type
 
-TODO: double linked list AND Circular linked list
+What I define it is not an array, even tho I set type hint as integer
+I did not make any validation on the passed value
+And it can  grow dinamically, so more of a concept of a list, not array
+
+TODO: double linked list AND Circular linked list AND some ARRAY IMPLEMENTATION
+
+REFERENCES: 
+iterators
+https://realpython.com/python-iterators-iterables/#coding-asynchronous-iterators
+https://docs.python.org/3/library/itertools.html
+https://www.datacamp.com/tutorial/python-iterators-generators-tutorial
 
 """
